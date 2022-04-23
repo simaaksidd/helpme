17355010824962a4639a722aae5fd0fd2a7916e8
Simaak Siddiqi <simaaksidd1@gmail.com>
2022-04-23 17:10:58 -0500

    commiy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+class App extends Component {
+  render() {
+    const coords = { lat: 29.7752, lng: -95.3103 };
+    return (
+      <div className="App">
+        <header className="App-header">
+          < img src={logo} className="App-logo" alt="logo" />
+          <h1 ClassName="App-title">Help Me!</h1>  
       </header>
-    </div>
-  );
+      <Map google={this.props.google} zoom={10} initialCenter={coords}>
+      <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+        </InfoWindow>
+      
+      </Map>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyCY0Z6N0L7GGmgnp0CiFVdxeVmmPvykEhg")
+})(App)
